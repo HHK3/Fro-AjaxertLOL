fc6e66b03d09896ba113c44ecd3ded7bfd89b163
HHK3 <joeltilon@live.nl>
2018-05-16 10:18:20 +0200

    Initial commit

---
+++ 3B/getuser.php
@@ -0,0 +1,36 @@
+<?php
+$q = $_REQUEST["q"];
+
+
+$con = mysqli_connect('localhost','root','','ajax');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"ajax");
+$sql="SELECT * FROM w3c_ajax_demo WHERE Voornaam LIKE '$q%' OR Achternaam LIKE '$q%'";
+$result = mysqli_query($con,$sql);
+
+echo "<table>
+<tr>
+<th>Voornaam</th>
+<th>Achternaam</th>
+<th>Leeftijd</th>
+<th>Woonplaats</th>
+<th>Streamer?</th>
+</tr>";
+
+
+
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['Voornaam'] . "</td>";
+    echo "<td>" . $row['Achternaam'] . "</td>";
+    echo "<td>" . $row['Leeftijd'] . "</td>";
+    echo "<td>" . $row['Woonplaats'] . "</td>";
+    echo "<td>" . $row['Streamer'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+mysqli_close($con);
